2011253d1bb3f72c29607fe1b275720f49b2c85f
Steven Joseph <stevolisisjoseph@gmail.com>
2023-11-24 10:15:59 +0100

    Routes imports changed to routes

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ require('dotenv').config();
 const mongoose=require('mongoose');
 const formidableMiddleware = require('express-formidable');
 const ejs=require("ejs");
-// const health_auth_route=require('./Routes/health_provider_routes/auth');
-// const provider_profile_route=require('./Routes/health_provider_routes/profile');
-// const patients_route=require('./Routes/health_provider_routes/patients');
-// const resources_route=require('./Routes/health_provider_routes/resources');
-// const patient_auth_route=require('./Routes/patient_routes/auth');
+const health_auth_route=require('./routes/health_provider_routes/auth');
+// const provider_profile_route=require('./routes/health_provider_routes/profile');
+// const patients_route=require('./routes/health_provider_routes/patients');
+// const resources_route=require('./routes/health_provider_routes/resources');
+// const patient_auth_route=require('./routes/patient_routes/auth');
 // const patient_profile_route=require('./routes/patient_routes/profile')
-// const medication_route=require('./Routes/patient_routes/medication');
-// const review_route=require('./Routes/review_route/index');
+// const medication_route=require('./routes/patient_routes/medication');
+// const review_route=require('./routes/review_route/index');
 
 
 
@@ -21,7 +21,7 @@ app.set('view engine','ejs');
 
 
 // //routes
-// app.use('/health_provider/auth',health_auth_route);
+app.use('/health_provider/auth',health_auth_route);
 // app.use('/health_provider/profile',provider_profile_route);
 // app.use('/health_provider/patients',patients_route);
 // app.use('/health_provider/resources',resources_route);
@@ -61,20 +61,20 @@ app.get('/',(req,res)=>{
 });
 
 
-app.get('/2',async(req,res)=>{
-	let name ="John Mike";
-	let email='john@gmail.com'
-	let passcode=38297;
+// app.get('/2',async(req,res)=>{
+// 	let name ="John Mike";
+// 	let email='john@gmail.com'
+// 	let passcode=38297;
 	
-	try{
-	let emailTemplate=await ejs.renderFile("views/welcome1.ejs",{
-        name: name,
-        email: email,
-        passcode: passcode
-	});
-	  res.send(emailTemplate);
-	}catch(err){
-	res.send('<h1>Error</h1>')
-	console.log(err)
-	}
-});
+// 	try{
+// 	let emailTemplate=await ejs.renderFile("views/welcome1.ejs",{
+//         name: name,
+//         email: email,
+//         passcode: passcode
+// 	});
+// 	  res.send(emailTemplate);
+// 	}catch(err){
+// 	res.send('<h1>Error</h1>')
+// 	console.log(err)
+// 	}
+// });
