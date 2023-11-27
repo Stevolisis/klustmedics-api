13f5be6152143b005fb4a7bf89f270470fb07bec
Steven Joseph <stevolisisjoseph@gmail.com>
2023-11-27 21:43:05 +0100

    get_medications update

--- controllers/health_provider_controller/medications/index.js
+++ controllers/health_provider_controller/medications/index.js
@@ -4,7 +4,7 @@ const Medications = require("../../../models/medication_schema");
 exports.get_medications=async (req,res)=>{
     try{
         const { id }=req.params;
-        const medications_get=await Medications.findOne({patient_id:id}).sort({_id:-1});
+        const medications_get=await Medications.find({patient_id:id}).sort({_id:-1});
         res.status(200).json({status:'success',data:medications_get});
     }catch(err){
         console.log(err);
